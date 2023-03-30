3542d84e22d50bc263fd0dafdc583150edc1fb23
Stefano <tepo@hotmail.it>
2023-03-30 21:54:59 +0200

    cut the array of followers

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,19 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const itemsPerPage = 12;
+  const pages = Math.ceil(followers.length / itemsPerPage);
+
+  // const newFollowers = Array.from({length: pages}, (_, index) => {
+  //   const start = index * itemsPerPage;
+  //   return followers.slice(start, start + itemsPerPage)
+  // });
+
+  const newFollowers = [];
+  for (let i = 0; i < pages; i++) {
+    const element = followers.slice(itemsPerPage * i, itemsPerPage * (i + 1));
+    newFollowers.push(element);
+  }
+
+return newFollowers;
+}
 
 export default paginate
