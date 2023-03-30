4512e8d08539458301187581cc9d787c7f16d529
Stefano <tepo@hotmail.it>
2023-03-30 22:20:29 +0200

    add next and prev page buttons

--- src/App.js
+++ src/App.js
@@ -18,6 +18,26 @@ function App() {
     setPage(index);
   }
 
+  const nextPage = () => {
+  setPage((oldPage) => {
+    let nextPage = oldPage + 1;
+    if (nextPage > data.length - 1) {
+      nextPage = 0;
+    }
+    return nextPage;
+  })
+}
+
+  const prevPage = () => {
+    setPage((oldPage) => {
+    let prevPage = oldPage - 1;
+    if (prevPage < 0) {
+      prevPage = data.length - 1;
+    }
+    return prevPage;
+  })
+}
+
   return (
     <main>
       <div className="section-title">
@@ -30,21 +50,27 @@ function App() {
             return <Follower key={follower.id} {...follower} />;
           })}
         </div>
-        {!loading && 
-        <div className="btn-container">
-          {data.map((item, index) => {
-            return (
-              <button
-                key={index}
-                className={`page-btn ${index === page ? 'active-btn' : ''}`}
-                onClick={() => handlePage(index)}
-              >
-                {index + 1}
-              </button>
-            );
-          })}
-        </div>
-        }
+        {!loading && (
+          <div className="btn-container">
+            <button className="prev-btn" onClick={prevPage}>
+              prev
+            </button>
+            {data.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  className={`page-btn ${index === page ? "active-btn" : ""}`}
+                  onClick={() => handlePage(index)}
+                >
+                  {index + 1}
+                </button>
+              );
+            })}
+            <button className="next-btn" onClick={nextPage}>
+              next
+            </button>
+          </div>
+        )}
       </section>
     </main>
   );
